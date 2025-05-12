3d0322a1bd411b2431501c560d1e51a68dca919e
Renan Vaz <renan_vaz1@hotmail.com>
2025-05-12 10:58:34 -0300

    Estruturas HTML, CSS e Js criadas e iniciadas

---
+++ script.js
@@ -0,0 +1,35 @@
+let B7Validator = {
+    handleSubmit:(event)=>{
+        event.preventDefault()
+        
+        let send = true;
+
+        let inputs = form.querySelectorAll('input')
+
+        for(let i = 0; i < inputs.length; i++) {
+            let input = inputs[i]
+            let checkInput = B7Validator.checkInput(input)
+            if(check !== true) {
+                send = false
+            }
+        }
+
+        send = false;
+
+        if(send) {
+            form.submit()
+        } 
+
+    },
+
+    checkInput:(input)=> {
+        let rules = input.getAttribute('data-rules')
+        if(rules !== null) {
+            rules = rules.split('|')
+        }
+        return true
+    }
+}
+
+let form = document.querySelector('.b7validator')
+form.addEventListener('submit', B7Validator.handleSubmit )
